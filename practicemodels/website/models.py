319051862b8f4e9315f025476cc287a68403b46e
mansoor ahmed <mansoor1122000@gmail.com>
2023-09-15 18:12:49 +0500

    fix conflicts

--- practicemodels/website/models.py
+++ practicemodels/website/models.py
@@ -130,9 +130,4 @@ class ProxyBook(Book):
 class Author(models.Model):
     id = models.AutoField( primary_key=True)
     name = models.CharField(max_length=100)
-<<<<<<< Updated upstream
-=======
 
-    def __str__(self):
-        return self.name
->>>>>>> Stashed changes
